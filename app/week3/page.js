4af2b10dc062b9b298bc74511dccb41d85ecf9c3
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2023-10-05 00:06:30 -0600

    Finished week 3
    Got everything to work.

--- app/week3/page.js
+++ app/week3/page.js
@@ -1,15 +1,12 @@
 import Link from "next/link";
-
+import { ItemList } from "./Item-List";
+import { ItemCard } from "./Item";
 export default function Week3() {
+
+
     return(
-        <html>
-            <head><title>Week3</title></head>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div class="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-                <h1 class="text-4xl font-bold">Week 3 Shopping List</h1>
-                <br></br>
-            </div>
-            </main>
-      </html>
+        <main>
+            <ItemList />
+        </main>
     )
 };
