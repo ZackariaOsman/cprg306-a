71625b6f62a37d243991b9995abbf5195a8b902e
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2023-10-15 22:05:17 -0600

    Week 4 better
    made the quantity bar bigger

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -43,7 +43,7 @@ export default function NewItem() {
                     {/**Quantity**/}
                     <div className='mt-2.5'>
                         <label className='text-xl'>Quantity: </label>
-                        <input className='h-10 w-12 appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
+                        <input className='h-10 appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline'
                             type="number"
                             required
                             value={quantity}
