d05610ec794212dd9bd938607b9ac7686fa7d021
Zackaria Osman <Zackaria.Osman@edu.sait.ca>
2023-11-12 18:47:54 -0600

    Update firebase.js

--- app/week8/_utils/firebase.js
+++ app/week8/_utils/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+//import { initializeApp } from "firebase/app";
 import { NEXT_PUBLILC_FIREBASE_API_KEY, NEXT_PUBLILC_FIREBASE_AUTH_DOMAIN, NEXT_PUBLILC_FIREBASE_PROJECT_ID, NEXT_PUBLILC_FIREBASE_STORAGE_BUCKET, NEXT_PUBLILC_FIREBASE_MESSAGING_SENDER_ID, NEXT_PUBLILC_FIREBASE_APP_ID } from ".env.local";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
